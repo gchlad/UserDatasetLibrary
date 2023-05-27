a658ba878ac11b168fd95887e40915b2575133b8
gchlad <g.chladkova@email.cz>
2023-05-27 16:16:42 +0200

    .

--- UserDatasetLibrary/Program.cs
+++ UserDatasetLibrary/Program.cs
@@ -33,6 +33,28 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.RegisterTransientCrudServices();
 
+//
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+//
+
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
